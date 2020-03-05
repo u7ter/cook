5470ab63cc2208ac4a8765dc37b9ed7bfb868f9b
Maksim Mysak <maksimmysak@gmail.com>
2020-03-06 00:17:21 +0200

    add api

--- api/server.go
+++ api/server.go
@@ -1,17 +1,19 @@
 package api
 
 import (
-"fmt"
-"log"
-_"os"
+	"fmt"
+	"github.com/maksimmysak/cook/api/controllers"
+	"github.com/maksimmysak/cook/api/seed"
+	"log"
+	"os"
 
-"github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 //"github.com/victorsteven/fullstack/api/controllers"
 //"github.com/victorsteven/fullstack/api/seed"
 )
 
-//var server = controllers.Server{}
-//
+var server = controllers.Server{}
+
 func init() {
 	// loads values from .env into the system
 	if err := godotenv.Load(); err != nil {
@@ -29,10 +31,10 @@ func Run() {
 		fmt.Println("We are getting the env values")
 	}
 
-	//server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
+	server.Initialize(os.Getenv("DB_DRIVER"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"), os.Getenv("DB_NAME"))
 
-	//seed.Load(server.DB)
+	seed.Load(server.DB)
 
-	//server.Run(":8080")
+	server.Run(":8080")
 
 }
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/victorsteven/fullstack
+module github.com/maksimmysak/cook
 
 go 1.13
 
